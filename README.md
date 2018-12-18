d2938c0f8be5d9e4d494531e33e0ddeddd40ae48
bilger-progress <bilger.yahov@progress.com>
2018-12-18 17:19:55 +0200

    Set-up Push for iOS and Android. Add some description on how to start-up the app.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # knv-push-ns123
 Testing Kinvey Push Notifications for a NativeScript application.
+
+To set-up:
+1. Kinvey AppKey/Secret
+1. Username/PW
+1. Team ID in `build.xconfig`
--- knv-push-ns123/src/app/item/items.component.ts
+++ knv-push-ns123/src/app/item/items.component.ts
@@ -1,21 +1,89 @@
 import { Component, OnInit } from "@angular/core";
-
-import { Item } from "./item";
-import { ItemService } from "./item.service";
+import { Kinvey } from "kinvey-nativescript-sdk";
+import { Push } from "kinvey-nativescript-sdk/push";
 
 @Component({
     selector: "ns-items",
     moduleId: module.id,
     templateUrl: "./items.component.html",
 })
-export class ItemsComponent implements OnInit {
-    items: Item[];
 
-    // This pattern makes use of Angular’s dependency injection implementation to inject an instance of the ItemService service into this class.
-    // Angular knows about this service because it is included in your app’s main NgModule, defined in app.module.ts.
-    constructor(private itemService: ItemService) { }
+export class ItemsComponent implements OnInit {
+    constructor() {
+        // Use the component constructor to inject providers.
+    }
 
     ngOnInit(): void {
-        this.items = this.itemService.getItems();
+        // Initialize Kinvey.
+        Kinvey.init({
+            appKey: 'xxx',
+            appSecret: 'xxx'
+        });
+
+        // Test connection.
+        Kinvey.ping()
+            .then((response) => {
+                console.log(`Kinvey Ping Success. Kinvey Service is alive, version: ${response.version}, response: ${response.kinvey}`);
+            })
+            .catch((error) => {
+                console.log(`Kinvey Ping Failed. Response: ${JSON.stringify(error)}`);
+            });
+
+        // Check for an active user.    
+        const activeUser = Kinvey.User.getActiveUser();
+        if (!activeUser) {
+            Kinvey.User.login('xxx', 'xxx')
+                .then((user: Kinvey.User) => {
+                    this.registerPushNotifications();
+                })
+                .catch((error: Kinvey.BaseError) => {
+                    console.log(`Kinvey login Failed. Response: ${JSON.stringify(error)}`);
+                });
+        } else {
+            this.registerPushNotifications();
+        }
+    }
+
+    registerPushNotifications() {
+        Push.register({
+            android: {
+                senderID: '765209267254'
+            },
+            ios: {
+                alert: true,
+                badge: true,
+                sound: true,
+                interactiveSettings: {
+                    actions: [{
+                        identifier: 'READ_IDENTIFIER',
+                        title: 'Read',
+                        activationMode: "foreground",
+                        destructive: false,
+                        authenticationRequired: true
+                    }, {
+                        identifier: 'CANCEL_IDENTIFIER',
+                        title: 'Cancel',
+                        activationMode: "foreground",
+                        destructive: true,
+                        authenticationRequired: true
+                    }],
+                    categories: [{
+                        identifier: 'READ_CATEGORY',
+                        actionsForDefaultContext: ['READ_IDENTIFIER', 'CANCEL_IDENTIFIER'],
+                        actionsForMinimalContext: ['READ_IDENTIFIER', 'CANCEL_IDENTIFIER']
+                    }]
+                },
+            },
+            notificationCallback: (message: any) => {
+                alert("Push Notifications Message: " + JSON.stringify(message));
+            }
+        })
+            .then((deviceToken: string) => {
+                alert("Push Notifications Device Token: " + JSON.stringify(deviceToken));
+            })
+            .catch((error: Error) => {
+                alert("Push Notifications Error. Please check logs!");
+                console.error("Push Notifications Error: " + JSON.stringify(error));
+            });
     }
 }
